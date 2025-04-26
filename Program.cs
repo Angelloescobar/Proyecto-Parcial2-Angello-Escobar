5fbabcc6a6b6b79d0addef5a6955b0ddbe5e4855
Angelloescobar <aescobarg21@miumg.edu.gt>
2025-04-25 23:57:22 -0600

    proyecto

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Proyct2doParcial
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm()); // Asegúrate de que la clase MainForm esté definida en tu proyecto
+        }
+    }
+}
